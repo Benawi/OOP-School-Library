5e9be09c424e6f49c20162148726bd93f4aa0d9b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-01 22:55:02 +0300

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,19 @@
+require_relative './person'
+
+class Teacher < Person
+  def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+# Create a object with age < 18 and parent permission == false so if the method
+# is correctly overridden the return should be true
+
+teacher = Teacher.new(name: 'Matilda', age: 17, parent_permission: false, specialization: 'math teacher')
+
+puts teacher.can_use_services?
