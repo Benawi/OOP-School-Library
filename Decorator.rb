dca7605968638097925805e7db5392daa15dc9bd
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-03 00:18:34 +0300

    fix linters e=bug

--- Decorator.rb
+++ Decorator.rb
@@ -1,11 +1,9 @@
 require_relative 'nameable'
-
 class Decorator < Nameable
   def initialize(nameable)
     super()
     @nameable = nameable
   end
-
   def correct_name
     @nameable.correct_name
   end
