90b6a509893b61bebf327f9a214eb0c8ef15bc71
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-03 00:03:32 +0300

    fix bugs

--- person.rb
+++ person.rb
@@ -1,32 +1,26 @@
 require_relative 'nameable'
-class Person
-  def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = Random.rand(1..10_000)
+
+class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(parent_permission: true, name: 'Unknown', age: nil)
+    super()
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  @corrector = Corrector.new
-  attr_accessor :name, :age
-  attr_reader :id
+
+  def correct_name
+    @name
+  end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
-  def validate_name
-    @name = @corrector.correct_name(@name)
-  end
-
   private
-
   def of_age?
-    @age >= 18
+    @age.to_i >= 18
   end
 end
-test = Person.new(name: 'benawi', age: 18)
-
-puts test.name
-
-test.validate_name
-
-puts test.name
