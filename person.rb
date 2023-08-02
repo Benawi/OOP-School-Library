79f22b603ba62870c33858909ffea88ad15206d8
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-02 23:22:58 +0300

    Implement a method correct_name that capitalizes the output of @nameable.correct_name

--- person.rb
+++ person.rb
@@ -1,3 +1,4 @@
+require 'corrector'
 class Person
   def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..10_000)
@@ -5,12 +6,15 @@ class Person
     @age = age
     @parent_permission = parent_permission
   end
-
+  @corrector = Corrector.new
   attr_accessor :name, :age
   attr_reader :id
 
   def can_use_services?
     is_of_age? || @parent_permission
+  end
+  def validate_name
+    @name = @corrector.correct_name(@name)
   end
 
   private
