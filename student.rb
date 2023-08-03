25ccdc2a51e801c9d6087b8fc1851e3d16a9a903
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-03 10:33:48 +0300

    fix linters bug

--- student.rb
+++ student.rb
@@ -1,17 +1,15 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
 
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
