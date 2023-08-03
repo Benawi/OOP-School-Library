1eeb7b07b62ed0bf12db6beaf09bb501beb14d60
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-03 09:17:45 +0300

    Fix linters bugs

--- rental.rb
+++ rental.rb
@@ -1,7 +1,7 @@
 class Rental
   attr_accessor :date
   attr_reader :book, :person
-  
+
   def initialize(date, book, person)
     @date = date
     @book = book
--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   attr_accessor :classroom
-  
+
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
