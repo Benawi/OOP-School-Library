26e870f272ecf796a4417a148e1e4726f79098a2
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-03 08:28:35 +0300

    Create the many-to-many relationship

--- person.rb
+++ person.rb
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
--- student.rb
+++ student.rb
@@ -1,9 +1,11 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
