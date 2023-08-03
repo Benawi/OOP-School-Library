fd70bb89a19ce1d4a0cbd9eea003157849d2c74d
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-03 08:47:04 +0300

    Fix linters bugs

--- classroom.rb
+++ classroom.rb
@@ -1,10 +1,12 @@
 class Classroom
   attr_accessor :label
   attr_reader :students
+
   def initialize(label)
     @label = label
     @students = []
   end
+  
   def add_student(student)
     student.classroom = self
     @students << student
