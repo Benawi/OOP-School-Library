4dda6e91afcd13023273b0af1389e34ba6ed8c17
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-03 08:39:11 +0300

    Fix linters bugs

--- rental.rb
+++ rental.rb
@@ -1,14 +1,12 @@
 class Rental
-    attr_accessor :date
-    attr_reader :book, :person
+  attr_accessor :date
+  attr_reader :book, :person
   
-    def initialize(date, book, person)
-      @date = date
-      @book = book
-      @person = person
-  
-      @book.add_rental(self)
-  
-      @person.add_rental(self)
-    end
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+    @book.add_rental(self)
+    @person.add_rental(self)
+  end
 end
