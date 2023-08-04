4ca27c822c6ecc3e2178d8c07a9ca1b6a66e200b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-04 06:54:16 +0300

    define a method which display menu

--- app.rb
+++ app.rb
@@ -10,4 +10,17 @@ class App
     @people = []
   end
 
+  def display_menu
+    puts "\nOptions:"
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List all rentals for a given person id'
+    puts '7. Quit'
+    print 'Enter the option number: '
+  end
+
+
 end
