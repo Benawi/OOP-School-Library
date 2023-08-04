05b46bfe439566ba8c476bc632fb1237aedd7027
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-04 07:11:14 +0300

    define a method which create student

--- app.rb
+++ app.rb
@@ -44,4 +44,21 @@ class App
     end
   end
 
+  def create_student
+    print "Enter the student's age: "
+    age = gets.chomp.to_i
+
+    print "Enter the student's name: "
+    name = gets.chomp
+
+    print 'Has parent permission? [Y/N]: '
+    parent_permission_input = gets.chomp.downcase
+    parent_permission = %w[y yes].include?(parent_permission_input)
+
+    student = Student.new(name: name, age: age, parent_permission: parent_permission)
+    @people << student
+
+    puts "Student created successfully! (ID: #{student.id})"
+  end
+
 end
