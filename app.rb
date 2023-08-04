fb1fd8ec94735ac0f84a0d20f32703020ad5fc5f
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-04 07:14:05 +0300

    define create book method

--- app.rb
+++ app.rb
@@ -77,4 +77,16 @@ class App
     puts "Teacher created successfully! (ID: #{teacher.id})"
   end
 
+  def create_book
+    print "Enter the book's title: "
+    title = gets.chomp
+    print "Enter the book's author: "
+    author = gets.chomp
+
+    book = Book.new(title, author)
+    @books << book
+
+    puts 'Book created successfully!'
+  end
+
 end
