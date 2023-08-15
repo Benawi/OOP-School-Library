ef3763edd98abf31529e7e7df7aadf4192ec65cb
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-15 11:56:11 +0300

    Add method to load books from the json

--- book.rb
+++ book.rb
@@ -18,3 +18,8 @@ def self.save_books_to_json(books)
   books_data = books.map(&:to_hash)
   File.write('books.json', JSON.pretty_generate(books_data))
 end
+
+def self.load_books_from_json
+  books_data = JSON.parse(File.read('books.json'))
+  books_data.map { |data| Book.new(data['title'], data['author']) }
+end
