a963ee41e903e360b725410154d7776540a028ce
sahlminkok <sahlminkok7@gmail.com>
2023-08-15 16:00:02 +0100

    Add save_rentals_to_json and load_rentals_from_json methods

--- data/data_handler.rb
+++ data/data_handler.rb
@@ -1,8 +1,9 @@
 require 'json'
 
 class DataHandler
-  def initialize(data_manager)
+  def initialize(data_manager, rentals_json_file)
     @data_manager = data_manager
+    @rentals_json_file = rentals_json_file
   end
 
   def save_people_to_json(people)
@@ -30,5 +31,33 @@ class DataHandler
   rescue StandardError => e
     puts "Error loading from JSON: #{e.message}"
     []
+  end
+
+  def save_rentals_to_json(people)
+    rentals_data = people.map do |person|
+      person.rentals.map do |rental|
+        {
+          person_id: person.id,
+          book_title: rental.book.title,
+          rental_date: rental.date
+        }
+      end
+    end.flatten
+
+    @data_manager.save_to_json(@rentals_json_file, rentals_data)
+  end
+
+  def load_rentals_from_json(people, books)
+    return unless File.exist?(@rentals_json_file)
+  
+    rentals_data = JSON.parse(File.read(@rentals_json_file))
+    rentals_data.each do |rental_info|
+      person = people.find { |p| p.id == rental_info['person_id'] }
+      book = books.find { |b| b.title == rental_info['book_title'] }
+      next unless person && book
+  
+      rental = Rental.new(rental_info['rental_date'], book, person)
+      person.add_existing_rental(rental)
+    end
   end
 end
