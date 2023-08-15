0f0f3cff3b3506617f9e441245986028773dab5d
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-15 12:06:06 +0300

    Add method to find book by titile

--- book.rb
+++ book.rb
@@ -12,22 +12,26 @@ class Book
     @rentals << rental
     rental
   end
-end
 
-def self.save_books_to_json(books)
-  books_data = books.map(&:to_hash)
-  File.write('books.json', JSON.pretty_generate(books_data))
-end
+  def self.save_books_to_json(books)
+    books_data = books.map(&:to_hash)
+    File.write('books.json', JSON.pretty_generate(books_data))
+  end
 
-def self.load_books_from_json
-  books_data = JSON.parse(File.read('books.json'))
-  books_data.map { |data| Book.new(data['title'], data['author']) }
-end
+  def self.load_books_from_json
+    books_data = JSON.parse(File.read('books.json'))
+    books_data.map { |data| Book.new(data['title'], data['author']) }
+  end
 
-def to_hash
-  {
-    'title' => @title,
-    'author' => @author,
-    'rentals' => @rentals.map { |rental| { 'date' => rental.date } }
-  }
+  def to_hash
+    {
+      'title' => @title,
+      'author' => @author,
+      'rentals' => @rentals.map { |rental| { 'date' => rental.date } }
+    }
+  end
+
+  def self.find_by_title(books, title)
+    books.find { |book| book.title == title }
+  end
 end
