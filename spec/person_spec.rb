6dc38ff0b66f745f4ed31ecb58b01fe5207f62ac
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-16 13:33:22 +0300

    Add unit tests for can_use_services? method in Person'

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -9,10 +9,29 @@ RSpec.describe Person do
       expect(person.age).to eq(25)
       expect(person.instance_variable_get(:@parent_permission)).to eq(true)
     end
+  end
 
+  describe '#correct_name' do
     it 'Returns correct name of the person created' do
       person = Person.new(name: 'Everlyn')
       expect(person.correct_name).to eq('Everlyn')
+    end
+  end
+
+  describe '#can_use_services' do
+    it 'returns true if person age > 18' do
+      person = Person.new(age: 20)
+      expect(person.can_use_services?).to eq(true)
+    end
+
+    it 'returns true if person has parent_permission' do
+      person = Person.new(age: 5, parent_permission: true)
+      expect(person.can_use_services?).to eq(true)
+    end
+    
+    it 'returns false if the person age < 18 && parent_permission: false' do
+    person = Person.new(age: 15, parent_permission: false)
+    expect(person.can_use_services?).to eq(false)
     end
   end
 end
