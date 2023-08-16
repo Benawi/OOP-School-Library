87892e5e24a85f86ff8d5e19a2f17a1b167f5378
sahlminkok <sahlminkok7@gmail.com>
2023-08-16 16:03:43 +0100

    Add test for classroom class

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../classroom.rb'
+require_relative '../student.rb'
+
+describe Classroom do
+  classroom = Classroom.new('A')
+  student = Student.new(age: 19, name: 'Sahalu')
+
+  it 'should have a label' do
+    expect(classroom.label).to eq('A')
+  end
+
+  it 'should have a list of students' do
+    classroom.add_student(student)
+
+    expected_student_attributes = [{
+      name: 'Sahalu', age: 19
+    }]
+
+    actual_student_attributes = classroom.students.uniq { |s| [s.name, s.age] }.map { |s| { name: s.name, age: s.age } }
+    expect(actual_student_attributes).to match_array(expected_student_attributes)
+  end
+end
